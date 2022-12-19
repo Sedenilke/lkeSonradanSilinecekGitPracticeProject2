075b4c9e62e11ef8abdf8d9b84228e5100741d4d
Sedenilke <sedenilke@gmail.com>
2022-12-19 13:14:13 -0800

    Tgis is my first commit with classes: NewClass01 and NewClass02

---
+++ src/NewClass01.java
@@ -0,0 +1,5 @@
+public class NewClass01 {
+    public static void main(String[] args) {
+        System.out.println("Here is my new class. Hello!!!!");
+    }
+}
---
+++ src/NewClass02.java
@@ -0,0 +1,5 @@
+public class NewClass02 {
+    public static void main(String[] args) {
+        System.out.println("Here is my another class: NewClass02");
+    }
+}
